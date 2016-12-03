060cc9373281cf0daef5d8644158d836d7737a92
jipmoon <hyungjip@Hyungs-MacBook-Pro-2.local>
2016-12-03 00:24:26 -0800

    Draw the full grid on the board

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class Board : MonoBehaviour {
 
+	public Transform m_emptySprite;
+	public int m_height = 30;
+	public int m_width = 30;
+
+	public int m_header = 8;
 	// Use this for initialization
 	void Start () {
-	
+		DrawEmptyCells();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void DrawEmptyCells() {
+
+		// Check to see if we assigned prefab to emptySprite
+		if(m_emptySprite != null) {
+		// By using nested for-loop, it should hit every grid of the board
+		// Outer Loop that increment through the rows
+			for(int y = 0; y < m_height; y++) {
+
+				// Inner Loop that counts through each column
+				for(int x = 0; x < m_width; x++) {
+
+					// It handles the creation of each square
+					Transform clone;
+
+					//Vector3 takes one counter from x and one counter from y
+					clone = Instantiate(m_emptySprite, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+					clone.name = "Board Space ( x = " + x.ToString() + " , y =" + y.ToString() + ")";
+					clone.transform.parent = transform;
+				}
+			}
+		}
+		else {
+			
+			Debug.Log("WARNING! Please assign the emptySprite object!");
+		}
 	}
 }
